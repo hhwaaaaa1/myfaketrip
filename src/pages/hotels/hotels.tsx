4bf52c04ba01d8a4c19c265616c3578220949008
hwajeong <hhwaaaaa1@naver.com>
2020-07-01 23:18:58 +0900

    global style

--- src/pages/hotels/hotels.tsx
+++ src/pages/hotels/hotels.tsx
@@ -7,6 +7,28 @@ function Hotels(): JSX.Element {
     <>
       <GlobalHeader />
       <HotelSearchBar />
+      <style jsx global>{`
+        html {
+          font-size: 10px;
+          -webkit-text-size-adjust: 100%;
+          -webkit-font-smoothing: antialiased;
+          -webkit-tap-highlight-color: transparent;
+        }
+
+        body {
+          margin: 0;
+          font: 400 14px -apple-system, BlinkMacSystemFont, Malgun Gothic, Roboto, Helvetica, Arial, sans-serif;
+        }
+
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6 {
+          margin: 0;
+        }
+      `}</style>
     </>
   )
 }
